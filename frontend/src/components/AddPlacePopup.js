e8c0a6c3907a37ae61361357d47e71cdf4be1dcd
Anastasia Astashova <n-aneko23@yandex.ru>
2021-03-30 21:59:21 +0300

    ПР15

--- frontend/src/components/AddPlacePopup.js
+++ frontend/src/components/AddPlacePopup.js
@@ -1,44 +1,44 @@
-import React from 'react';
-// eslint-disable-next-line no-unused-vars
+import React from "react";
 import PopupWithForm from './PopupWithForm';
 
 export default function AddPlacePopup(props) {
-  const [place, setPlace] = React.useState('');
-  const [url, setUrl] = React.useState('');
+    const [place, setPlace] = React.useState('');
+    const [url, setUrl] = React.useState('');
 
-  // Обновляю поля после закрытия попапа
-  React.useEffect(() => {
-    if (props.onClose) {
-      setPlace('');
-      setUrl('');
+//Обновляю поля после закрытия попапа
+    React.useEffect(() => {
+        if (props.onClose) {
+            setPlace('');
+            setUrl('');
+        }
+    }, [props.onClose])
+
+//Изменяю значение поля "название"
+    function changePlace(e) {
+        setPlace(e.target.value);
     }
-  }, [props.onClose]);
 
-  // Изменяю значение поля "название"
-  function changePlace(e) {
-    setPlace(e.target.value);
-  }
+//Изменяю значение поля "ссылка"
+    function changeLink(e) {
+        setUrl(e.target.value);
+    }
 
-  // Изменяю значение поля "ссылка"
-  function changeLink(e) {
-    setUrl(e.target.value);
-  }
+//Обработчик события
+    function handleSubmit(e) {
+        e.preventDefault();
+        props.onAddPlace({
+            name: place,
+            link: url
+        })
+    }
 
-  // Обработчик события
-  function handleSubmit(e) {
-    e.preventDefault();
-    props.onAddPlace({
-      name: place,
-      link: url,
-    });
-  }
 
-  return (
+    return (
         <PopupWithForm name='place' title='Новое место' submit='Добавить' isOpen={props.isOpen} onClose={props.onClose} onClosePopup={props.onClosePopup} onSubmit={handleSubmit} >
             <input className="popup__data popup__data_place" id="form-place" type="text" placeholder="Название" value={place} onChange={changePlace} name="placeInput" required minLength="2" maxLength="30" />
             <span className= "popup__input-error" id='form-place-error' />
             <input className="popup__data popup__data_link" id="form-link" placeholder="Ссылка на картинку" value={url} onChange={changeLink} type="url" name="linkInput" required />
             <span className= "popup__input-error" id='form-link-error' />
         </PopupWithForm>
-  );
+    )
 }
--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,53 +1,49 @@
 const BASE_URL = 'https://auth.nomoreparties.co';
 
 export const register = (password, email) => {
-  return fetch(`${BASE_URL}/signup`, {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ password, email }),
-  })
-    .then((res) => {
-      if (!res.ok) {
-        return Promise.reject(`Ошибка: ${res.status}`);
-      }
-      return res.json();
-    });
-};
-
-export const authorize = (email, password) => {
-  return fetch(`${BASE_URL}/signin`, {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ email, password }),
-  })
-    .then((res) => {
-      if (!res.ok) {
-        // eslint-disable-next-line prefer-promise-reject-errors
-        return Promise.reject(`Ошибка: ${res.status}`);
-      }
-      return res.json();
-    });
-};
-
-export const getContent = (token) => {
-  return fetch(`${BASE_URL}/users/me`, {
-    method: 'GET',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${token}`,
-    },
-  })
-    .then((res) => {
-      res.json();
+    return fetch(`${BASE_URL}/signup`, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({password, email})
     })
-    .then((data) => {
-      return data;
+    .then((res) => {
+      if (!res.ok) {
+        return Promise.reject(`Ошибка: ${res.status}`); 
+    }
+    return res.json()
     });
-};
+    
+  };
+  
+  export const authorize = (email, password) => {
+    return fetch(`${BASE_URL}/signin`, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({email, password})
+    })
+    .then((res) => {
+      if (!res.ok) {
+        return Promise.reject(`Ошибка: ${res.status}`); 
+    }
+    return res.json()
+  });
+}
+
+  export const getContent = (token) => {
+    return fetch(`${BASE_URL}/users/me`, {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`,
+      }
+    })
+    .then(res => res.json())
+    .then(data => data)
+  }
